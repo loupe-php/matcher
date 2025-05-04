dc806c3d2baf690bb17f026b65be95273e2ef479
Philipp Daun <post@philippdaun.net>
2025-05-04 15:35:48 +0200

    Add tests for highlighting
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -7,8 +7,10 @@ namespace Loupe\Matcher\Tests;
 use Loupe\Matcher\Formatter;
 use Loupe\Matcher\FormatterOptions;
 use Loupe\Matcher\Matcher;
+use Loupe\Matcher\Tokenizer\Token;
 use Loupe\Matcher\Tokenizer\TokenCollection;
 use Loupe\Matcher\Tokenizer\Tokenizer;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class FormatterTest extends TestCase
@@ -21,7 +23,7 @@ class FormatterTest extends TestCase
     {
         $tokenizer = new Tokenizer();
 
-        $this->matcher = new Matcher($tokenizer);
+        $this->matcher = new Matcher($tokenizer, stopWords: ['a', 'of', 'the']);
         $this->queryTerms = $tokenizer->tokenize('test');
     }
 
@@ -78,5 +80,120 @@ class FormatterTest extends TestCase
         $result = $formatter->format('This is a test string.', $this->queryTerms, $options);
 
         $this->assertSame('This is a test string.', $result->getFormattedText());
+    }
+
+    public static function formattingProvider(): \Generator
+    {
+        yield 'No formatting' => [
+            'soul',
+            'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.',
+            'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.',
+        ];
+
+        yield 'Highlighting' => [
+            'soul',
+            'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole soul.',
+            'A wonderful serenity has taken possession of my entire [soul], like these sweet mornings of spring which I enjoy with my whole [soul].',
+            true,
+            '[',
+            ']',
+        ];
+
+        yield 'Highlighting with match at start' => [
+            'serenity',
+            'Serenity has taken possession of my entire soul.',
+            '[Serenity] has taken possession of my entire soul.',
+            true,
+            '[',
+            ']',
+        ];
+
+        yield 'Highlighting with match at end' => [
+            'soul',
+            'A wonderful serenity has taken possession of my entire soul.',
+            'A wonderful serenity has taken possession of my entire [soul].',
+            true,
+            '[',
+            ']',
+        ];
+
+        yield 'Highlighting with tags' => [
+            'my',
+            'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole soul.',
+            'A wonderful serenity has taken possession of <b>my</b> entire soul, like these sweet mornings of spring which I enjoy with <b>my</b> whole soul.',
+            true,
+            '<b>',
+            '</b>',
+        ];
+
+        yield 'Highlighting with text case' => [
+            'my wonderful soul',
+            'Wonderful serenity has taken possession. My entire soul of spring which I enjoy with my whole: Soul.',
+            '[Wonderful] serenity has taken possession. [My] entire [soul] of spring which I enjoy with [my] whole: [Soul].',
+            true,
+            '[',
+            ']',
+        ];
+
+        yield 'Highlighting with subwords' => [
+            'wonder',
+            'Wonderful serenity has taken possession of my entire soul, like a sweet morning wonder of spring.',
+            'Wonderful serenity has taken possession of my entire soul, like a sweet morning [wonder] of spring.',
+            true,
+            '[',
+            ']',
+        ];
+
+        yield 'Highlighting with token variants' => [
+            new TokenCollection(
+                [
+                    (new Token(0, 'my', 0, false, false, false)),
+                    (new Token(1, 'wonder', 3, false, false, false))->withVariants(['wonders']),
+                    (new Token(2, 'soul', 10, false, false, false))->withVariants(['souls']),
+                ],
+            ),
+            'A wonder of wonders has taken possession of my entire soul, like some souls\' mornings of spring.',
+            'A [wonder] of [wonders] has taken possession of [my] entire [soul], like some [souls]\' mornings of spring.',
+            true,
+            '[',
+            ']',
+        ];
+
+        yield 'Highlighting with stopwords' => [
+            'a meeting of souls',
+            'A meeting of souls has taken possession of a collection of souls, like a morning of sweet spring.',
+            '[A meeting of souls] has taken possession of a collection [of souls], like a morning of sweet spring.',
+            true,
+            '[',
+            ']',
+        ];
+    }
+
+    #[DataProvider('formattingProvider')]
+    public function testFormatting(
+        string|TokenCollection $query,
+        string $text,
+        string $expectedResult,
+        bool $enableHighlight = false,
+        string $highlightStartTag = '<em>',
+        string $highlightEndTag = '</em>',
+    ): void {
+        $options = (new FormatterOptions());
+        if ($enableHighlight) {
+            $options = $options->withEnableHighlight()
+                ->withHighlightStartTag($highlightStartTag)
+                ->withHighlightEndTag($highlightEndTag);
+        } else {
+            $options = $options->withDisableHighlight();
+        }
+
+        $query = $query instanceof TokenCollection
+            ? $query
+            : (new Tokenizer())->tokenize($query, stopWords: ['a', 'of', 'the'], includeStopWords: true);
+
+        $formatter = new Formatter($this->matcher);
+        $result = $formatter->format($text, $query, $options);
+
+        $this->assertSame($expectedResult, $result->getFormattedText());
     }
 }
