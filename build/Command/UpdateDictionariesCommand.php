c7f41c2e5bb12d2b83a93d673933fda4c4ba5633
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-01-12 11:22:45 +0100

    Allow building only one locale

--- build/Command/UpdateDictionariesCommand.php
+++ build/Command/UpdateDictionariesCommand.php
@@ -8,14 +8,15 @@ use Loupe\Matcher\Build\DictionaryBuilderInterface;
 use Loupe\Matcher\Build\Locale\Dutch;
 use Loupe\Matcher\Build\Locale\German;
 use Loupe\Matcher\Locale;
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(name: 'loupe-matcher:update_dictionaries', description: 'Updates all dictionaries')]
+#[AsCommand(name: 'update-dictionaries', description: 'Updates all dictionaries')]
 class UpdateDictionariesCommand
 {
-    public function __invoke(SymfonyStyle $io): int
+    public function __invoke(SymfonyStyle $io, #[Argument] string|null $locale = null): int
     {
         $builders = [
             new German(),
@@ -26,6 +27,10 @@ class UpdateDictionariesCommand
             if (!$builder instanceof DictionaryBuilderInterface) {
                 $io->error('All builders must implement the DictionaryBuilderInterface!');
                 return Command::FAILURE;
+            }
+
+            if ($locale !== null && !$builder->getLocale()->matches(Locale::fromString($locale))) {
+                continue;
             }
 
             $this->info($io, $builder->getLocale(), 'Building directory now.');
