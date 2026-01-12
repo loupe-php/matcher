7e9a729e9462111ca36f643ef81312b33205ca67
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-01-12 10:54:43 +0100

    Added 2 bytes prefix index for binary search buckets

--- src/Tokenizer/Decompounder/Dictionary/AbstractBinaryFileDictionary.php
+++ src/Tokenizer/Decompounder/Dictionary/AbstractBinaryFileDictionary.php
@@ -10,11 +10,15 @@ abstract class AbstractBinaryFileDictionary implements DictionaryInterface
 {
     private const MAX_LENGTH_IN_BYTES = 256;
 
+    private const U32_NONE = 0xFFFFFFFF;
+
     private string $blob = '';
 
     private int $count = 0;
 
     private string $index = '';
+
+    private string $prefixBuckets = '';
 
     /**
      * @param array<string> $dictionary
@@ -46,10 +50,25 @@ abstract class AbstractBinaryFileDictionary implements DictionaryInterface
             throw new \LogicException('This dictionary is not initalized yet.');
         }
 
-        // This algorithm performs a binary search on our blob string
-        $lowerIndex = 0;
-        $upperIndex = $this->count - 1;
+        // Use the first two bytes of the term to search in our prefix index for matching
+        // lower and upper bounds. This reduces lookup time drastically because we now have
+        // split up our entire index into a maximum of 65,536 buckets, and we thus only need to work within
+        // the matching bucket.
+        $byte0 = isset($term[0]) ? \ord($term[0]) : 0;
+        $byte1 = isset($term[1]) ? \ord($term[1]) : 0;
+        $key = ($byte0 << 8) | $byte1;
 
+        $bucket = unpack('Vlow/Vhigh', substr($this->prefixBuckets, $key * 8, 8));
+        $lowerIndex = $bucket['low'] ?? self::U32_NONE;
+
+        // If no term exists for that prefix2, we can return immediately
+        if ($lowerIndex === self::U32_NONE) {
+            return false;
+        }
+
+        $upperIndex = $bucket['high'] ?? -1;
+
+        // Standard binary search here
         while ($lowerIndex <= $upperIndex) {
             // Choose the middle index between current bounds
             $middleIndex = ($lowerIndex + $upperIndex) >> 1;
@@ -124,27 +143,31 @@ abstract class AbstractBinaryFileDictionary implements DictionaryInterface
     {
         $termsPath = $directory . '/terms';
         $indexPath = $directory . '/index';
+        $prefixPath = $directory . '/prefix_buckets';
         $blob = null;
-        $idx = null;
+        $index = null;
+        $prefixBuckets = null;
 
-        $load = function (bool $decode = true) use (&$load, $directory, $termsPath, $indexPath, &$blob, &$idx) {
+        $load = function (bool $decode = true) use (&$load, $directory, $termsPath, $indexPath, $prefixPath, &$blob, &$index, &$prefixBuckets) {
             $blob = @file_get_contents($termsPath);
-            $idx = @file_get_contents($indexPath);
+            $index = @file_get_contents($indexPath);
+            $prefixBuckets = @file_get_contents($prefixPath);
 
-            if ($decode && ($blob === false || $idx === false)) {
+            if ($decode && ($blob === false || $index === false || $prefixBuckets === false)) {
                 $this->decodeDictionary($directory);
                 $load(false);
             }
         };
         $load();
 
-        if (!\is_string($blob) || !\is_string($idx)) {
+        if (!\is_string($blob) || !\is_string($index) || !\is_string($prefixBuckets)) {
             throw new \RuntimeException('Cannot load blob from directory.');
         }
 
         $this->blob = $blob;
-        $this->index = $idx;
-        $this->count = intdiv(\strlen($idx), 4);
+        $this->index = $index;
+        $this->prefixBuckets = $prefixBuckets;
+        $this->count = intdiv(\strlen($index), 4);
     }
 
     private static function calculateCommonPrefixLengthInBytes(string $a, string $b): int
@@ -172,13 +195,20 @@ abstract class AbstractBinaryFileDictionary implements DictionaryInterface
             throw new \RuntimeException('Cannot open terms for writing');
         }
 
-        $idxOut = fopen($directory . '/index', 'wb');
-        if ($idxOut === false) {
+        $indexOut = fopen($directory . '/index', 'wb');
+        if ($indexOut === false) {
             throw new \RuntimeException('Cannot open index for writing');
+        }
+
+        $prefixBucketsOut = fopen($directory . '/prefix_buckets', 'wb');
+        if ($prefixBucketsOut === false) {
+            throw new \RuntimeException('Cannot open prefix_buckets for writing');
         }
 
         $previous = '';
         $position = 0;
+        $prefixRanges = [];
+        $termIndex = 0;
 
         while (!gzeof($directoryHandle)) {
             $header = $this->gzReadBytesExactly($directoryHandle, 2);
@@ -197,12 +227,40 @@ abstract class AbstractBinaryFileDictionary implements DictionaryInterface
             $term = substr($previous, 0, $prefixLen) . $suffix;
 
             // Our index file contains all positions as unsigned 32-bit integer (should be easily enough)
-            fwrite($idxOut, pack('V', $position));
+            fwrite($indexOut, pack('V', $position));
             fwrite($termsOut, $term . "\n");
+
+            $byte0 = isset($term[0]) ? \ord($term[0]) : 0;
+            $byte1 = isset($term[1]) ? \ord($term[1]) : 0;
+            $key = ($byte0 << 8) | $byte1;
+
+            // If we don't have this prefix yet, set lower and higher position the to the same value
+            if (!isset($prefixRanges[$key])) {
+                $prefixRanges[$key] = [$termIndex, $termIndex];
+            } else {
+                // Otherwise, update the higher value only (the range increases)
+                $prefixRanges[$key][1] = $termIndex;
+            }
+            $termIndex++;
 
             $position += \strlen($term) + 1;
             $previous = $term;
         }
+
+        // Write our prefix buckets index that contains the low and high index positions for all prefixes (the first 2 bytes)
+        // for faster lookups
+        for ($k = 0; $k < 65536; $k++) {
+            if (!isset($prefixRanges[$k])) {
+                fwrite($prefixBucketsOut, pack('V', self::U32_NONE) . pack('V', self::U32_NONE));
+            } else {
+                [$low, $high] = $prefixRanges[$k];
+                fwrite($prefixBucketsOut, pack('V', $low) . pack('V', $high));
+            }
+        }
+        fclose($prefixBucketsOut);
+        fclose($termsOut);
+        fclose($indexOut);
+        gzclose($directoryHandle);
     }
 
     /**
