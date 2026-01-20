3d077658a1abcaba50fdf7a5e65f104bd6a5b9ee
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-01-20 19:18:54 +0100

    English actually also has a lot of compound words

---
+++ build/Locale/English.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Loupe\Matcher\Build\Locale;
+
+use Loupe\Matcher\Build\DictionaryBuilder\AbstractKaikkiDictionaryBuilder;
+use Loupe\Matcher\Locale;
+
+class English extends AbstractKaikkiDictionaryBuilder
+{
+    public function getLocale(): Locale
+    {
+        return Locale::fromString('en');
+    }
+
+    protected function allowTerm(string $term, array $json): bool
+    {
+        // At least 3 letters total
+        if (!preg_match('/^[a-z]{3,}$/u', $term)) {
+            return false;
+        }
+
+        if (!$this->isAllowedPos($json, ['noun', 'verb', 'adj'])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function getDumpUrl(): string
+    {
+        return 'https://kaikki.org/dictionary/English/kaikki.org-dictionary-English.jsonl';
+    }
+}
