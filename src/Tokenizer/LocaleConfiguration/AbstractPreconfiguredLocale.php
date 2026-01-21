f06146cecb8feb4a7decfa8ac896a7fddc2a8704
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-01-21 12:37:16 +0100

    Introduce a memory cache with FIFO and use that in preconfigured locales

--- src/Tokenizer/LocaleConfiguration/AbstractPreconfiguredLocale.php
+++ src/Tokenizer/LocaleConfiguration/AbstractPreconfiguredLocale.php
@@ -6,7 +6,9 @@ namespace Loupe\Matcher\Tokenizer\LocaleConfiguration;
 
 use Loupe\Matcher\Tokenizer\Decompounder\Configuration;
 use Loupe\Matcher\Tokenizer\Decompounder\Decompounder;
+use Loupe\Matcher\Tokenizer\Decompounder\Dictionary\DictionaryInterface;
 use Loupe\Matcher\Tokenizer\Decompounder\Dictionary\FastSetDictionary;
+use Loupe\Matcher\Tokenizer\Decompounder\Dictionary\MemoryCacheDictionary;
 use Loupe\Matcher\Tokenizer\Normalizer\Normalizer;
 use Loupe\Matcher\Tokenizer\Normalizer\NormalizerInterface;
 use Loupe\Matcher\Tokenizer\Token;
@@ -25,12 +27,12 @@ abstract class AbstractPreconfiguredLocale implements LocaleConfigurationInterfa
         return $token->withAddedVariants($this->decompounder->decompoundTerm($token->getTerm(), $token->getLength()));
     }
 
-    public function getDictionary(): FastSetDictionary
+    public function getDictionary(): DictionaryInterface
     {
-        return new FastSetDictionary(
+        return new MemoryCacheDictionary(new FastSetDictionary(
             $this->getLocale(),
             __DIR__ . '/../../../dictionaries/' . $this->getLocale()->toString()
-        );
+        ), $this->getNumberOfCacheEntriesForMemoryCacheDictionary());
     }
 
     public function getNormalizer(): NormalizerInterface
@@ -39,4 +41,12 @@ abstract class AbstractPreconfiguredLocale implements LocaleConfigurationInterfa
     }
 
     abstract protected function getDecompounderConfiguration(): Configuration;
+
+    protected function getNumberOfCacheEntriesForMemoryCacheDictionary(): int
+    {
+        // Default to 15k entries. This should be a fair balance for fast lookups for a lot of terms
+        // while ensuring memory is low. Should end up being a max of 2 - 3 MB of RAM depending
+        // on the length of the terms.
+        return 15_000;
+    }
 }
