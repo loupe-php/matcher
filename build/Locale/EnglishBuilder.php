be8f8608736dee52cfc6e5da3f068a75bef20612
Philipp Daun <post@philippdaun.net>
2026-01-25 13:22:29 +0100

    Improve decomposition of ding suffix
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- build/Locale/EnglishBuilder.php
+++ build/Locale/EnglishBuilder.php
@@ -18,6 +18,7 @@ class EnglishBuilder extends AbstractKaikkiDictionaryBuilder
         'eld',
         'shi',
         'ting', // apparently the sound made when a small bell is struck, that makes no sense to decompose and it makes for bad splits as a lot of "ing" words end on "ting".
+        'ding', // similar to above
         'der', // interjection
         'kee', // alternative of "cow"
         'sch', // abbreviation of "school"
