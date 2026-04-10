4fd492919a9e40f998e3c59e723311845a75b44b
Philipp Daun <post@philippdaun.net>
2026-04-10 23:30:30 +0200

    Add tests for token lengths
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/MatcherTest.php
+++ tests/MatcherTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Loupe\Matcher\Tests;
 
 use Loupe\Matcher\Matcher;
+use Loupe\Matcher\Tokenizer\LocaleConfiguration\German;
 use Loupe\Matcher\Tokenizer\TokenCollection;
 use Loupe\Matcher\Tokenizer\Tokenizer;
 use PHPUnit\Framework\TestCase;
@@ -34,11 +35,44 @@ final class MatcherTest extends TestCase
         $this->assertSame(15, $spans[0]->getLength());
     }
 
+    public function testDecompositionKeepsCorrectPositions(): void
+    {
+        $text = 'das grosse kuenstlerinnengespraech war fuer die silvesternacht geplant';
+        $query = 'gespraech nacht';
+
+        $tokenizer = new Tokenizer(new German());
+        $matcher = new Matcher($tokenizer);
+        $matches = $matcher->calculateMatches($text, $query);
+        $spans = $matcher->calculateMatchSpans($text, $query, $matches);
+
+        $this->assertEquals(2, \count($matches->all()));
+        $this->assertSame([11, 34], [$spans[0]->getStartPosition(), $spans[0]->getEndPosition()]);
+        $this->assertSame([48, 62], [$spans[1]->getStartPosition(), $spans[1]->getEndPosition()]);
+
+        $words = array_map(fn ($t) => $t->getTerm(), $matches->all());
+        $this->assertEquals(['kuenstlerinnengespraech', 'silvesternacht'], $words);
+    }
+
     public function testEmptyTextReturnsEmptyCollection(): void
     {
         $result = $this->matcher->calculateMatches('', 'query');
         $this->assertInstanceOf(TokenCollection::class, $result);
         $this->assertCount(0, $result->all());
+    }
+
+    public function testNormalizationKeepsCorrectPositions(): void
+    {
+        $text = 'Das große Künstlerinnengespräch war für die Silvesternacht geplant';
+        $query = 'gespräch nacht';
+
+        $tokenizer = new Tokenizer(new German());
+        $matcher = new Matcher($tokenizer);
+        $matches = $matcher->calculateMatches($text, $query);
+        $spans = $matcher->calculateMatchSpans($text, $query, $matches);
+
+        $this->assertEquals(2, \count($matches->all()));
+        $this->assertSame([10, 31], [$spans[0]->getStartPosition(), $spans[0]->getEndPosition()]);
+        $this->assertSame([44, 58], [$spans[1]->getStartPosition(), $spans[1]->getEndPosition()]);
     }
 
     public function testSimpleMatch(): void
