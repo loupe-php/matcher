2716c8153366e2397131f80d20db9bdb25fcf4b7
Philipp Daun <post@philippdaun.net>
2026-06-03 11:40:12 +0200

    Update cropper tests

--- tests/Formatting/CropperTest.php
+++ tests/Formatting/CropperTest.php
@@ -12,59 +12,59 @@ class CropperTest extends TestCase
 {
     public static function cropHighlightedTextProvider(): \Generator
     {
-        yield 'Cropping with too much context and no change' => [
+        yield 'Cropping with too much context' => [
             'A wonderful serenity has <em>taken</em> possession of my entire soul, like these sweet mornings have <em>taken</em> all spring.',
-            'A wonderful serenity has <em>taken</em> possession of my entire soul, like these sweet mornings have <em>taken</em> all spring.',
+            '…wonderful serenity has <em>taken</em> possession of my entire… like these sweet mornings have <em>taken</em> all spring.',
         ];
 
         yield 'Cropping with less context and change' => [
             'A wonderful serenity has <em>taken</em> possession of my entire soul, like these sweet mornings have <em>taken</em> all spring.',
-            '…serenity has <em>taken</em> possession…mornings have <em>taken</em> all spring…',
-            25,
+            '…wonderful serenity has <em>taken</em> possession of my…sweet mornings have <em>taken</em> all spring.',
+            35,
         ];
 
         yield 'Cropping around single term in center' => [
             'A wonderful serenity has taken possession of my entire <em>soul</em>, like these sweet mornings have taken all spring.',
-            '…serenity has taken possession of my entire <em>soul</em>, like these sweet mornings have taken…',
+            '…possession of my entire <em>soul</em>, like these sweet mornings…',
         ];
 
         yield 'Cropping around repeating term' => [
             'A wonderful serenity has taken possession of my entire <em>soul</em>, like these sweet mornings of spring which I enjoy with my whole <em>soul</em>. I am alone, and feel the charm of existence in this spot, which was created for the bliss of a <em>soul</em> like mine.',
-            '…serenity has taken possession of my entire <em>soul</em>, like these sweet mornings of spring which I enjoy with my whole <em>soul</em>. I am alone, and feel the charm of existence…which was created for the bliss of a <em>soul</em> like mine.',
+            '…possession of my entire <em>soul</em>, like these sweet mornings…I enjoy with my whole <em>soul</em>. I am alone, and feel the…which was created for the bliss of a <em>soul</em> like mine.',
         ];
 
         yield 'Cropping around multiple terms' => [
             'A wonderful serenity has taken possession of my entire <em>soul</em>, like these sweet mornings of spring which I enjoy with my whole being. I am alone, and feel the charm of existence in this spot, which was created for the <em>bliss</em> of a heart like mine.',
-            '…serenity has taken possession of my entire <em>soul</em>, like these sweet mornings of spring…this spot, which was created for the <em>bliss</em> of a heart like mine.',
+            '…possession of my entire <em>soul</em>, like these sweet mornings…which was created for the <em>bliss</em> of a heart like mine.',
         ];
 
         yield 'Cropping at start' => [
             '<em>Wonderful</em> serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole soul.',
-            '<em>Wonderful</em> serenity has taken possession of…',
+            '<em>Wonderful</em> serenity has taken possession of my entire…',
         ];
 
         yield 'Cropping at end' => [
             'Wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole <em>panorama</em>.',
-            '…spring which I enjoy with my whole <em>panorama</em>.',
+            '…mornings of spring which I enjoy with my whole <em>panorama</em>.',
         ];
 
         yield 'Cropping with custom length' => [
             'Wonderful serenity has taken possession of my <em>entire</em> soul, like these sweet mornings of spring which I enjoy with my <em>whole</em> panorama.',
-            '…my <em>entire</em> soul…with my <em>whole</em> pano…',
-            15,
+            '…possession of my <em>entire</em> soul, like these…enjoy with my <em>whole</em> panorama.',
+            30,
         ];
 
         yield 'Cropping with custom marker' => [
             'Wonderful serenity has taken possession of my <em>entire</em> soul, like these sweet mornings of spring which I enjoy with my <em>whole</em> panorama.',
-            ' --- possession of my <em>entire</em> soul, like --- enjoy with my <em>whole</em> panorama.',
-            25,
+            ' --- possession of my <em>entire</em> soul, like these --- spring which I enjoy with my <em>whole</em> panorama.',
+            40,
             ' --- ',
         ];
 
         yield 'Cropping with custom highlight tags' => [
             'Wonderful serenity has taken <mark>possession</mark> of my <em>entire</em> soul, like these sweet mornings of <mark>spring</mark> which I enjoy with my <em>whole</em> panorama.',
-            '…taken <mark>possession</mark> of my <em>entire</em>…mornings of <mark>spring</mark> which I enjoy…',
-            25,
+            '…serenity has taken <mark>possession</mark> of my <em>entire</em>…sweet mornings of <mark>spring</mark> which I enjoy with…',
+            40,
             '…',
             '<mark>',
             '</mark>',
