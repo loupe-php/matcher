cda4a67ba7249adeb369a11c1b50616c4ab21359
Philipp Daun <post@philippdaun.net>
2026-06-04 12:49:51 +0200

    Look up stopwords from index

--- src/StopWords/InMemoryStopWords.php
+++ src/StopWords/InMemoryStopWords.php
@@ -9,16 +9,21 @@ use Loupe\Matcher\Tokenizer\Token;
 class InMemoryStopWords implements StopWordsInterface
 {
     /**
+     * @var array<string, true>
+     */
+    private array $stopWordsIndex = [];
+
+    /**
      * @param array<string> $stopWords
      */
     public function __construct(
         private array $stopWords = []
     ) {
-
+        $this->stopWordsIndex = array_fill_keys($stopWords, true);
     }
 
     public function isStopWord(Token $token): bool
     {
-        return \in_array($token->getTerm(), $this->stopWords, true);
+        return isset($this->stopWordsIndex[$token->getTerm()]);
     }
 }
